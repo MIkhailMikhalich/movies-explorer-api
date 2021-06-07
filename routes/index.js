4269dfc4ade74fc6695f5955d0dd2522b479c399
Mikhail Sholeninov <medvedsholeninov@yandex.ru>
2021-06-07 16:59:44 +0300

    Fix: Добавить проверку запроса перед передачи его контроллеру

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const users = require('./users.js');
 const movies = require('./movies.js');
 const { postUser, login, logout } = require('../controllers/users.js');
@@ -6,8 +7,19 @@ const NotFound = require('../errors/not-found-err.js');
 
 const auth = require('../middlewares/auth.js');
 
-router.post('/signin', login);
-router.post('/signup', postUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+    name: Joi.string().min(2).max(30).required(),
+  }),
+}), postUser);
 router.use(auth);
 router.use('/users', users);
 router.use('/movies', movies);
