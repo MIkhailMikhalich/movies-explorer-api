e9f788f74771bcecf1ab01e292e5eb54cc7e8823
Mike <72221688+MIkhailMikhalich@users.noreply.github.com>
2021-07-02 19:47:35 +0300

    Исправлен ответ на запрос выдачи фильмов

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,9 +1,9 @@
-const objectId = require('mongodb').ObjectID;
-const regExp = require('../constants/constants.js');
-const NotFound = require('../errors/not-found-err.js');
-const IncorrectData = require('../errors/incorrect-data.js');
-const DoNotHavePermission = require('../errors/do-not-have-permisson.js');
-const Movie = require('../models/movie.js');
+const objectId = require("mongodb").ObjectID;
+const regExp = require("../constants/constants.js");
+const NotFound = require("../errors/not-found-err.js");
+const IncorrectData = require("../errors/incorrect-data.js");
+const DoNotHavePermission = require("../errors/do-not-have-permisson.js");
+const Movie = require("../models/movie.js");
 
 module.exports.postMovie = (req, res, next) => {
   const {
@@ -22,17 +22,18 @@ module.exports.postMovie = (req, res, next) => {
   const owner = req.user._id;
   const links = [image, trailer, thumbnail];
   if (!image || !regExp.test(links[0])) {
-    throw new IncorrectData('Переданы некорректные данные');
+    throw new IncorrectData("Переданы некорректные данные");
   }
   regExp.test(links[1]);
   if (!trailer || !regExp.test(links[1])) {
-    throw new IncorrectData('Переданы некорректные данные');
+    throw new IncorrectData("Переданы некорректные данные");
   }
   regExp.test(links[2]);
   if (!thumbnail || !regExp.test(links[2])) {
-    throw new IncorrectData('Переданы некорректные данные');
+    throw new IncorrectData("Переданы некорректные данные");
   }
-  if (!nameRU || !nameEN) throw new IncorrectData('Переданы некорректные данные');
+  if (!nameRU || !nameEN)
+    throw new IncorrectData("Переданы некорректные данные");
   Movie.create({
     country,
     director,
@@ -53,24 +54,32 @@ module.exports.postMovie = (req, res, next) => {
 
 module.exports.getMovies = (req, res, next) => {
   Movie.find({})
-    .then((movies) => res.send(movies))
+    .then((movies) => {
+      const filteredMovies = movies.filter((item) => {
+        if (item.owner === req.user._id) return item;
+      });
+      res.send(filteredMovies);
+    })
+
     .catch(next);
 };
 module.exports.deleteMovieById = (req, res, next) => {
-  if (!objectId.isValid(req.params.movieid)) throw new IncorrectData('Переданы некоректные данные');
+  if (!objectId.isValid(req.params.movieid))
+    throw new IncorrectData("Переданы некоректные данные");
   Movie.findById(req.params.movieid)
     .then((movie) => {
-      if (!movie) throw new NotFound('Данный фильм не найден');
+      if (!movie) throw new NotFound("Данный фильм не найден");
       const movieID = String(movie.owner);
       const userID = req.user._id;
       if (userID === movieID) {
         Movie.findByIdAndRemove(req.params.movieid)
           .then(() => {
-            if (!movie) throw new NotFound('Данный фильм не найден');
+            if (!movie) throw new NotFound("Данный фильм не найден");
             return res.send({ data: movie });
           })
           .catch(next);
-      } else throw new DoNotHavePermission('Фильм принадлежит другому пользователю');
+      } else
+        throw new DoNotHavePermission("Фильм принадлежит другому пользователю");
     })
     .catch(next);
 };
